645d09e2620904e0a12488433e9a8a29ef2dfaab
Ricardo Polanski Alves <rpolanski@live.com>
2024-08-28 18:59:40 -0300

    test

---
+++ src/test/java/appium_test/BaseTest.java
@@ -0,0 +1,55 @@
+package appium_test;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.time.Duration;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import com.google.common.collect.ImmutableMap;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+
+public class BaseTest {
+
+    public AndroidDriver driver;
+    public AppiumDriverLocalService service;
+
+    @BeforeClass
+    public void ConfigureAppium() throws MalformedURLException, URISyntaxException {
+        service = new AppiumServiceBuilder()
+                .withAppiumJS(new File("C:/Users/ercirci/AppData/Roaming/npm/node_modules/appium/build/lib/main.js"))
+                .withIPAddress("127.0.0.1").usingPort(4723).build();
+
+        service.start();
+
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setDeviceName("Polanski_Emulator");
+        options.setApp(
+                "C:/Users/ercirci/Projects/Appium/appium/src/test/java/appium_test/resources/ApiDemos-debug.apk");
+
+        driver = new AndroidDriver(new URI("http://127.0.0.1:4723/").toURL(), options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+    }
+
+    public void LongPressGesture(WebElement ele) {
+        driver.executeScript("mobile: longClickGesture", ImmutableMap.of("elementId", ((RemoteWebElement) ele).getId(),
+                "duration", 2000));
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+        service.stop();
+    }
+
+}
---
+++ src/test/java/appium_test/parentClassDemo.java
@@ -0,0 +1,20 @@
+package appium_test;
+
+public class parentClassDemo {
+    
+    String colour = "red";    
+
+    public void Gear() {
+        System.out.println("Gear code is implemented");
+    }
+
+    public void Breaks() {
+        System.out.println("Break code is implemented");
+    }
+
+    public void AudioSystem() {
+        System.out.println("Break code is implemented");
+    }
+    public static void main(String[] args) {
+    }
+}
